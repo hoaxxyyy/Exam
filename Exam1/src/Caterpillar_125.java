1966434b0b759bcaede05bbf4676182ac6f4ff77
admin <admin@Bcalab4-m03>
2022-06-14 09:15:38 +0530

    commit1

---
+++ Exam1/src/Caterpillar_125.java
@@ -0,0 +1,17 @@
+
+public class Caterpillar_125 {
+	
+	int moves, cabbages;
+	
+	Caterpillar_125(int moves, int cabbages)
+	{
+		this.moves = moves;
+		this.cabbages= cabbages;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
